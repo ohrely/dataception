6b0f039dae8f03d157606aefd9a61384e5e0d261
ohrely <rebaely@gmail.com>
2015-12-03 09:46:39 -0800

    Added structures.py.  Next: pull together data.

---
+++ structures.py
@@ -0,0 +1,26 @@
+"""Inception, represented through data structures in python.
+
+For fun and practice.
+"""
+
+# Intception
+
+# Stringception
+
+# Listception
+
+# Tupception
+
+# Setception
+
+# Dictception
+
+# Graphception
+
+# Treeception
+
+# Stackception
+
+# Queueception
+
+# Linkception
