e3a18ef477fd8b2256a1632dc5e7012313c12206
ohrely <rebaely@gmail.com>
2015-12-04 17:29:05 -0800

    Intception is just 528491 (for now).

--- structures.py
+++ structures.py
@@ -4,29 +4,49 @@ For fun and practice.
 """
 
 # Intception
+code = 528491
+
+print code
+
 
 # Stringception
-
 paradox = "a dream within a dream"
 
-within = paradox.join(paradox.split("a dream"))
+plot = paradox.join(paradox.split("a dream"))
 
-print within
+print plot
+
 
 # Listception
 
+
+
 # Tupception
+
+
 
 # Setception
 
+
+
 # Dictception
+
+
 
 # Graphception
 
+
+
 # Treeception
+
+
 
 # Stackception
 
+
+
 # Queueception
+
+
 
 # Linkception
