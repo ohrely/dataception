d2857c832f0fd9a7253931d5f51265e3a67ea7f1
ohrely <rebaely@gmail.com>
2015-12-10 02:32:58 -0800

    Listception done.

--- structures.py
+++ structures.py
@@ -3,32 +3,47 @@
 For fun and practice.
 """
 
-# Intception
-dream = 528
-team = 491
-
-print dream, team
+from model import connect_to_db, db
+from model import Level, Player, PlayerLevel
+import doctest
 
 
-# Stringception
-paradox = "a dream within a dream"
+def intception():
+    dream = 528
+    team = 491
 
-plot = paradox.join(paradox.split("a dream"))
-
-print plot
+    print dream, team
 
 
-# Listception
+def stringception():
+    paradox = "a dream within a dream"
 
+    plot = paradox.join(paradox.split("a dream"))
+
+    print plot
+
+
+def listception(level):
+    """creates a list of the team members at the given level.
+
+    >>> listception(1)
+    [u'Ariadne', u'Arthur', u'Dom', u'Eames', u'Fischer', u'Saito', u'Yusuf']
+    """
+    level_players = db.session.query(PlayerLevel.player).filter(PlayerLevel.level_num == level).all()
+
+    level_list = []
+
+    for player in level_players:
+        level_list.append(player[0])
+
+    print level_list
 
 
 # Tupception
 
 
-
 # Setception
-
-
+# do set math with listception.
 
 # Dictception
 # create a dictionary of dictionaries.
@@ -36,25 +51,24 @@ print plot
 # at each level, the dreamer's value is a dict of the team members on that level.
 # build recursively?
 
-
 # Graphception
-
-
+# map who knows who - will need another association table.
 
 # Treeception
-
-
 
 # Stackception
 # dreamers' experience of dream order.
 # some variable better be named non_rien_nest_rien.
 
-
-
 # Queueception
-
-
 
 # Linkception
 # doubly-linked list of dream order.
 # why doubly linked?  because dreamers have to kick back out of the dreams in order.
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
+
+    doctest.testmod(verbose=True)
